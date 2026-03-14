c9871a63472661dd538d0e6a9cf99ab3a5be3deb
Karim Abouelela <karim@abouelela.ch>
2026-03-14 14:25:04 +0100

    Include questions router in backend app

--- app/main.py
+++ app/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 
 from app.api.routes_backtesting import router as backtesting_router
 from app.api.routes_forecasts import router as forecasts_router
+from app.api.routes_questions import router as questions_router
 
 
 app = FastAPI(title="Prognostic API")
 
+app.include_router(questions_router)
 app.include_router(forecasts_router)
 app.include_router(backtesting_router)
 
